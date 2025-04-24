483b80dda8f176c351a42ebbfff2f6ec53a2956d
nicob02 <66340166+nicob02@users.noreply.github.com>
2025-04-24 19:56:41 -0400

    Update test.py

--- test.py
+++ test.py
@@ -9,21 +9,21 @@ import os
 
 
 
-delta_t = 1 # Mess around with this
-#poisson_params = 6.28318
-#poisson_params = 12.56637  #4pi, initial test case, change later.
-poisson_params = 25.13274  #8pi, initial test case, change later.
-#func_name = 'rfa'
+
 out_ndim = 1
 
 dens=65
 ckptpath = 'checkpoint/simulator_%s.pth' % Func.func_name    #FIGURE THIS OUT
 device = torch.device(0)
 
-func_main = Func(delta_t=delta_t, params=poisson_params)
+func_main = Func(eps=(4.0,2.0,1.0),
+    k  =(20.0,10.0, 5.0),
+    center=(0.5,0.5),
+    r1=0.15,
+    r2=0.30,
+    bc_tol=1e-3
+)
 
-bc1 = func_main.boundary_condition
-ic = func_main.init_condition
 
 mesh = ElectrodeMesh(ru=(1, 1), lb=(0, 0), density=65)
 graph = mesh.getGraphData()
@@ -37,20 +37,15 @@ test_steps = 20
 test_config = parse_config()
 
 #model = kwargs['model'] # Extracts the model's dictioanry with the weights and biases values
-setattr(test_config, 'poisson_params', poisson_params)
-setattr(test_config, 'delta_t', delta_t)
 setattr(test_config, 'device', device)   
-setattr(test_config, 'ic', ic)
-setattr(test_config, 'bc1', bc1)
 setattr(test_config, 'model', model)
 setattr(test_config, 'test_steps', test_steps)
 setattr(test_config, 'NodeTypesRef', ElectrodeMesh.node_type_ref)
-#setattr(test_config, 'name', func_name)
 setattr(test_config, 'ndim', out_ndim)
 setattr(test_config, 'graph_modify', func_main.graph_modify)
 setattr(test_config, 'graph', graph)
 setattr(test_config, 'density', dens)
-
+setattr(test_config, 'func_main', func_main)
       
 
 #-----------------------------------------
