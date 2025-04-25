96eb55dada3d7d0df9d83ebb8cf1672a5f389860
nicob02 <66340166+nicob02@users.noreply.github.com>
2025-04-25 19:41:49 -0400

    Update test.py

--- test.py
+++ test.py
@@ -41,7 +41,7 @@ model = msgPassing(
     ndim=out_ndim,
     device=device,
     fourier_mapping_size=64,
-    fourier_scale=1.0,
+    fourier_scale=0.2,
     model_dir=ckptpath
 )
 model.load_model(ckptpath)
