feba994ba28df15284d9bc59c4080173ebeb2d9d
nicob02 <66340166+nicob02@users.noreply.github.com>
2025-04-26 17:35:09 -0400

    Update train.py

--- train.py
+++ train.py
@@ -33,7 +33,7 @@ model = msgPassing(
     ndim=out_ndim,
     device=device,
     fourier_mapping_size=64,
-    fourier_scale=0.1,
+    fourier_scale=0.01,
     model_dir=ckptpath
 )
 model.to(device)
